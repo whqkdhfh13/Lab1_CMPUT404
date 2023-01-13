d3fa7bfaab2e065f2e65631a7c0877dec8802bb4
Junhyeon Jayden Cho <whqkdhfh13@gmail.com>
2023-01-13 16:44:13 -0700

    curl.10

--- lab1.py
+++ lab1.py
@@ -6,4 +6,8 @@ py -c "import requests as r; print(r.__version__)"
 """
 
 # print(requests.__version__)
-print(requests.get('http://google.com/').text)
+url_google = 'http://google.com/'
+url_myscript = 'https://raw.githubusercontent.com/whqkdhfh13/Lab1_CMPUT404/main/lab1.py'
+url_to_use = url_myscript
+
+print(requests.get(url_myscript).text)
